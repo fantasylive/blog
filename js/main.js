28bebcdba0bdb0f7b91581c7c4a77972ffa9835c
fantasy_workmode <worker@work.com>
2024-04-12 14:38:15 +0800

    使用Docusaurus重新构建blog

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import {leadHover,showPageYOffset} from './leadBox.js';
-import {searchFocus,leftPart2ButtonHover,fixedLeftBox} from './leftBox.js';
-window.onload = function() {
-    leadHover();
-    searchFocus();
-    leftPart2ButtonHover();
-    // showPageYOffset();
-    fixedLeftBox();
-}
